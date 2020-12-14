bde7ebbfff09b3dec27cab50ca4c518384840445
Emiri Begic <emiri.begic@gmail.com>
2020-12-14 11:06:57 +0100

    modified: script.js and challenge for sec 9

--- 09-Data-Structures-Operators/starter/part-9-challenge.js
+++ 09-Data-Structures-Operators/starter/part-9-challenge.js
@@ -1,5 +1,51 @@
 'use strict';
 
+////////////////////////////////////
+// Challenge 4
+
+/*
+// test data
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+// 1. Get the user input data - OK
+const btn = document.querySelector('button');
+const arr = [];
+
+// convert them into lowercase - toLowerCase()
+// put them into an array, 5 diff ones - replace new lines into ',' and split with ','
+// ------ iterate ------
+// replace _ with space - OK
+// remove beginning & end spaces - OK
+// convert them into camelCase
+
+// 5 different strings
+// into 5 different arrays
+
+btn.addEventListener('click', function () {
+  const userInput = document.querySelector('textarea').value;
+  const items = userInput.split('\n');
+  console.log(items);
+
+  for (const [i, item] of items.entries()) {
+    const [first, second] = item.toLowerCase().trim().split('_');
+    // console.log(first, second);
+    const output = `${first}${second[0].toUpperCase()}${second.slice(1)}`;
+
+    console.log(`${output.padEnd(20)}${'💪'.repeat(i + 1)}`);
+  }
+});
+
+/*
+////////////////////////////////////
+// Challenge 1-3
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -40,3 +86,146 @@ const game = {
     team2: 6.5,
   },
 };
+*/
+
+/*
+////////////////////////////////////
+// Challenge 3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+
+// 2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3 TODO Review needed
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4
+// const first45 = '[FIRST HALF]';
+// const second45 = '[SECOND HALF]';
+// for (const [key, value] of gameEvents) {
+//   key < 45
+//     ? console.log(`${first45} ${key}: ${value}`)
+//     : console.log(`${second45} ${key}: ${value}`);
+// }
+// Official answer
+for (const [key, value] of gameEvents) {
+  const half = key <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${key}: ${value}`);
+}
+*/
+
+/*
+////////////////////////////////////
+// Challenge 2
+
+const odds = game.odds;
+
+// 1
+// const players = [...game.scored];
+// for (const [i, player] of players.entries()) {
+//   console.log(`Goal ${i + 1}: ${player}`);
+// }
+
+// Official answer
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+// 2
+const odd = Object.values(odds);
+console.log(odd);
+
+let sum = 0;
+for (const value of odd) {
+  sum += value;
+}
+const avrg = sum / odd.length;
+console.log(`Average odd: ${avrg}`);
+
+// 3 - TODO how to print team name???
+const entries = Object.entries(odds);
+// console.log(entries);
+
+for (const [team, oddValue] of entries) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of victory ${teamStr}: ${oddValue}`);
+}
+// game[team] to get value (team name) of team1 and team2
+
+// 4
+const scorers = {};
+scorers.Gnarby = 1;
+scorers.Hummels = 1;
+scorers.Lewandowski = 2;
+console.log(scorers);
+*/
+
+/*
+////////////////////////////////////
+// Challenge 1
+
+// 1 - Review needed (destructuring)
+// const players1 = [...game.players[0]];
+// const players2 = [...game.players[1]];
+// console.log(players1, players2);
+// Officail answer
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2 - OK
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3 - OK
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4 - OK
+const subPlayers1 = ['Thiago', 'Coutinho', 'Perisic'];
+const players1Final = [...players1, ...subPlayers1];
+console.log(players1Final);
+
+// 5 - Review neede (destructuring)
+// const team1 = game.odds.team1;
+// const draw = game.odds.x;
+// const team2 = game.odds.team2;
+// console.log(team1, draw, team2);
+// Official answer
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6
+const printGoals = function (...players) {
+  for (let i = 0; i < players.length; i++) console.log(players[i]);
+  console.log(`${players.length} goals were scored`);
+};
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7 Review neede - short circuiting
+// const win = team1 > team2 || team1 < team2;
+// console.log(win);
+team1 < team2 && console.log('Team 1 is more likele to win');
+team1 > team2 && console.log('Team 2 is more likele to win');
+*/
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
+// ES6 enhanced object literals - property names can be computed as below, not hard coding like thu, fri, sat...
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const hours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -7,45 +24,377 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // ES6 enhanced object literals - no need to write like hours; hours, for outer object
+  hours,
 
+  // ES6 enhanced object literals - no need to write function keyword and : for methods like ones from one below
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
+
+/*
+/////////////////////////////////////////
+// Working with Strings - Part 3
+
+// Split and join
+console.log('a+bery+nice+string'.split('+')); // split elements by the ordered string into an array
+console.log('Emilia Lala Puripuri Purikyua'.split(' '));
+const [firstName, lastName] = 'Emilia Lala'.split(' ');
+console.log(firstName, lastName); // Emilia Lala
+
+const newName = ['Ms.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName); // Ms. Emilia LALA
+
+const capitalizeName = function (name) {
+  const names = name.split(' '); // split and put the into an array
+  const newNames = []; // create an empty array to put modified name
+  for (const n of names) {
+    // newNames.push(n[0].toUpperCase() + n.slice(1));
+    newNames.push(n.replace(n[0], n[0].toUpperCase())); // above and here are same result
+  }
+  console.log(newNames.join(' ')); // convert into strings with spaces
+};
+
+capitalizeName('emilia mau lala purikyua');
+capitalizeName('pau punjara punta buta');
+
+// Padding (to add symbols and letters before/after particular string)
+const msg = 'Go to gate 23!';
+console.log(msg.padStart(25, '+').padEnd(30, '+')); // padEnd is total length, + before msg and msg itself are length of 25
+console.log('Maumau'.padStart(25, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  // const str = String(number);
+  const str = number + ''; // + makes everything string
+  const last = str.slice(-4); // takes last 4 digits
+  return last.padStart(str.length, '*'); // padded with * except last 4 digits
+};
+
+console.log(maskCreditCard(271286491283));
+
+// Repeat
+const msg2 = 'Bad weather... All Departures Delayer... ';
+console.log(msg2.repeat(5)); // repeating 5 times
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+*/
+
+/*
+/////////////////////////////////////////
+// Working with Strings - Part 2
+
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnAS'; // should print as Jonas
+const passengerLower = passenger.toLowerCase(); // first comvert them into lowercase
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1); // make the first letter uppercase and add lowercase
+console.log(passengerCorrect);
+
+const fixPassengerName = function (name) {
+  const lowercase = name.toLowerCase();
+  const correct = lowercase[0].toUpperCase() + lowercase.slice(1);
+  console.log(correct);
+};
+
+fixPassengerName('mAuMAu');
+fixPassengerName('dAVoR');
+fixPassengerName('eLIzaBetH');
+
+// Comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = ' Hello@Jonas.Io \n';
+// const lowerEmail = email.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = email.toLowerCase().trim();
+console.log(normalizedEmail);
+
+// // Checking emails
+// const userInput = prompt('Enter your email address 💌');
+// console.log(userInput);
+
+// const checkEmail = function (email) {
+//   const correctEmail = email.toLowerCase().replace(/\s/g, '');
+//   if (email === correctEmail) {
+//     alert('Thank you!');
+//     // console.log(email);
+//   } else {
+//     alert('Wrong email address, type again!');
+//   }
+//   // console.log(
+//   //   email === correctEmail ? email : 'Wrong email address, type again!'
+//   // );
+// };
+
+// checkEmail(userInput);
+
+// Replacing
+const priceGB = '288,97€'; // option + shift + 2 = euro symbol
+const priceUS = priceGB.replace('€', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replaceAll('door', 'gate')); //
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320')); // true
+console.log(plane.includes('boeing')); // false
+console.log(plane.startsWith('Air')); // true
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase(); // Always better convert user input into lowercase
+  if (baggage.includes('knife') || baggage.includes('gun'))
+    console.log('You are not allowed on board⛔️');
+  else console.log('Welcome aboard💁🏻‍♀️');
+};
+
+checkBaggage('I have a laptop, some food, and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got osme snacks and a Gun for protection');
+*/
+
+/*
+/////////////////////////////////////////
+// Working with Strings - Part 1
+
+const airline = 'TAP Air Portugal';
+const plen = 'A320';
+
+console.log(airline.indexOf('r')); // returns 6 (count spaces as well)
+console.log(airline.lastIndexOf('r')); // returns 10 (count spaces as well)
+console.log(airline.indexOf('portugal')); // returns -1 cos case sensitive (can't find)
+
+// Slice method - does not change original string
+console.log(airline.slice(4)); // returs Air Portigal, 4 is indexed nunmber where slice method starts to extract string
+console.log(airline.slice(4, 10)); // returs Air PO, 4 is the start and 10 is the end, end is no included
+// extracted strings (num of letters) are always end - beginning (10-4=6)
+
+console.log(airline.slice(0, airline.indexOf(' '))); // returns TAP, starts with index 0 and end with the firdst space
+console.log(airline.slice(airline.lastIndexOf(' ') + 1)); // returns Portugal, starts after the last space
+console.log(airline.slice(-2)); // returns al, starts from the end (right side)
+console.log(airline.slice(1, -1)); // returns AP Air Portuga, starts from the end (right side)
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😩');
+  else console.log('You got lucky 😎');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+*/
+
+/*
+/////////////////////////////////////////
+// Maps
+const rest = new Map(); // easy way to use is first create a empty Map
+rest.set('name', 'Classico Italiano'); //.set method is like .add method in Sets, 1st element is the name of the key and 2nd is the value
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal')); // up until here, this Map has above data and logs them as well
+
+// Chaining .set method
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+// Read data from Maps, use .get method
+console.log(rest.get('name')); // pass in the name of the key
+console.log(rest.get('true'));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close'))); // inside of () means that is time greater than 11 and less than 23 = true
+
+console.log(rest.has('categories')); // if rest has 'categories' = true
+rest.delete(2); // delete 'Lisbon, Portugal'
+// rest.clear(); // delete all the elements
+console.log(rest);
+console.log(rest.size); // 7
+
+// Arrays or Objects as Map keys
+
+// rest.set([1, 2], 'Test');
+// console.log(rest.get([1, 2])); // returns 'undefined' because [1,2] inside of .set method and the one inside of .get method are not same!!! They do not refer to the same place in the memory.
+
+// To get 'Test' by passing the name of the key, first define the array
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.get(arr));
+
+// HTML element as Map keys
+console.log(rest.set(document.querySelector('h1'), 'Heading'));
+
+// Another way to create Maps
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'], // first: name, second: value
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(hours));
+const hoursMap = new Map(Object.entries(hours));
+console.log(hoursMap);
+
+// Iterate map
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = 3;
+// const answer = Number(prompt('Your answer'));
+console.log(answer);
+// const check =
+//   answer === question.get('correct') ? question.get(true) : question.get(false);
+// console.log(check);
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+*/
+
+/*
+/////////////////////////////////////////
+// Sets
+const orderSet = new Set(['Pizza', 'Pasta', 'Pasta', 'Risotto', 'Pizza']);
+console.log(orderSet); // returns {"Pizza", "Pasta", "Risotto"}
+
+console.log(new Set('Mau')); // returns {"M", "a", "u"}
+
+console.log(orderSet.size); // how many elements = 3
+console.log(orderSet.has('Pizza')); // if orderSet has 'Pizza' = true
+console.log(orderSet.has('Bread')); // false
+orderSet.add('Garlic Bread'); // add a new element
+orderSet.add('Garlic Bread'); // only one 'Garlic Bread' will be added
+orderSet.delete('Risotto'); // delete 'Risotto' from orderSet
+// orderSet.clear(); // delete all elements in the Set
+console.log(orderSet);
+
+for (const order of orderSet) console.log(order);
+
+// Example - remove duplicated elements over an array, create a Set and convert Set to array
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)]; // convert Set to array (spread operator!!! can be used with Set as well)
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+); // count how many kind of strings = 3
+console.log(new Set('jonasschmedtmann').size); // count how many letters = 11
+*/
+
+/*
+/////////////////////////////////////////
+// Optional chaining
+// To get property NAMES of object
+const properties = Object.keys(hours);
+console.log(properties); // returns an array ["thu", "fri", "sat"]
+
+let openStr = ` We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr); // returns openStr with thu, fri, sat,
+
+// To get property VALUES of object
+const values = Object.values(hours);
+console.log(values); // returns an array with values
+
+// To get entire object (property NAMES and VALUES together)
+const entries = Object.entries(hours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+*/
+
+/*
+/////////////////////////////////////////
+// Optional chaining
+if (restaurant.hours && restaurant.hours.mon)
+  console.log(restaurant.hours.mon.open);
+
+// WITH optional chaining (better code than above)
+console.log(restaurant.hours.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.hours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`); // If the particular day exists, logs open time
+}
+
+// Methods WITH optional chaining
+console.log(restaurant.order?.(0, 1)) ?? 'Method does not exist';
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays WITH optional chaining
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+console.log(users[0]?.name ?? 'User array empty');
+*/
+
+/*
+/////////////////////////////////////////
+// For of Loop
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) console.log(item); // Focaccia Bruschetta and so on...
+
+// Old school way
+for (const item of menu.entries()) console.log(`${item[0] + 1}: ${item[1]}`); // 1: Focaccia 2: Bruschetta
+
+// Better way
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`); // 1: Focaccia 2: Bruschetta
+}
+*/
 
 /*
 /////////////////////////////////////////
