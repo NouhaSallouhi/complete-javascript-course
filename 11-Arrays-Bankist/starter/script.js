d6805f07ec18ed637f6dbeb871d423706375c12e
Emiri Begic <emiri.begic@gmail.com>
2021-01-07 22:32:30 +0100

    Add concat method examples

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -100,3 +100,8 @@ arr = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['j', 'i', 'h', 'g', 'f'];
 console.log(arr2.reverse()); // ["f", "g", "h", "i", "j"]
 console.log(arr2); // ["f", "g", "h", "i", "j"]
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters); // ['a', 'b', 'c', 'd', 'e', "f", "g", "h", "i", "j"]
+console.log([...arr, ...arr2]); // same as above
