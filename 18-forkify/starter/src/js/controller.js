77ce574e63ff0134ef5241e179e3d2cf7928f07b
Emiri Begic <emiri.begic@gmail.com>
2021-03-23 00:13:57 +0100

    Refactor with class syntax

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,13 +1,35 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1. Loading recipe
+    await model.loadRecipe(id);
+    // const { recipe } = model.state;
+
+    // 2. Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// PUBLISHER AND SUBSCRIBER PATTERN
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
